9d714b7ef09daca0b842b707b7b131b864a0254b
sajadht <sajadht@gmail.com>
2026-07-06 03:09:23 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kajboom-v1";
+const CACHE = "kajboom-v2";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", (e) => {
